640c22e57bbd1ce0921a8593fd5b8b5f032eba10
Jair Rillo Junior <jairrillo@Jairs-MacBook-Pro.local>
2016-01-01 17:52:36 -0200

    First Commit

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/authenticate', function(req, res, next) {
+	var username = req.body.username;
+	var password = req.body.password;
+	if (username == "batman" && password == "1234") {
+		//Return the User
+		var user = {
+			id: 1,
+			name: "Batman",
+			username: batman,
+			password: "1234",
+			email: "batman@superhero.com",
+			isActive: true,
+			insertDate: new Date()
+		}
+		res.status(200).json(user);
+
+	} else {		
+		res.status(400).send({errorMessage: "Username and/or Password do not exist"});	
+	}
+  	
+});
+
+module.exports = router;
